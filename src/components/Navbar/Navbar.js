63c0711b2439cba5f983ba5e166d498d599217a9
Dawid Kłos <klosdawid12@gmail.com>
2022-12-04 19:52:30 +0000

    Initial commit. Set up files structure, normalize default styles. Add first component Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../../styles/Navbar/navbar.scss";
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <p >Hello, hello</p>
+        </div>
+    )
+}
+
+export default Navbar;
