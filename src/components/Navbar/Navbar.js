11cf5bac5d8970bb565043c07136088a84f52606
Dawid Kłos <klosdawid12@gmail.com>
2022-12-06 18:48:24 +0000

    Add HTML structure for Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+import Logo from "../../images/logo.svg";
 import "../../styles/Navbar/navbar.scss";
 
 const Navbar = () => {
     return(
-        <div className="navbar">
-            <p >Hello, hello</p>
-        </div>
+        <header className="navbar" id="home">
+            <img className="navbar__logo" src={Logo} alt="Rosa Reinigung company logo" />
+            <nav className="navbar__nav">
+                <a href="#home">Home</a>
+                <a href="#services">Dienstleistungen</a>
+                <a href="#reviews">Bewertungen</a>
+                <a href="#gallery">Fotogalerie</a>
+                <a href="#contact">Kontakt</a>
+                <button type="button">
+                    <a href="#form">Terminvereinbarung</a>
+                </button>
+            </nav>
+        </header>
     )
 }
 
