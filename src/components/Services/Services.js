4cb9c48e36acf44491599d1d597a0d01c848da03
Dawid Kłos <klosdawid12@gmail.com>
2022-12-30 15:23:09 +0000

    Fix issues with images in each Service and Headings sizes

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import "../../styles/services.scss";
-import Lines from "../../images/img-services-detail.svg";
+import Lines from "../../images/img-line-services.svg";
+import Detail from "../../images/img-services-detail.svg";
 import HouseCleaning from "./HouseCleaning/HouseCleaning";
 import CorporateCleaning from "./CorporateCleaning/CorporateCleaning";
 import WindowCleaning from "./WindowCleaning/WindowCleaning";
@@ -12,8 +13,9 @@ const Services = () => {
     return(
         <section className="services">
             <h2 className="services__heading">Unsere Dienstleistungen für Dich.</h2>
-            <div className="services__image">
-                <img className="services-image__lines" src={Lines} alt="A woman falling through mess and cable on the floor" />
+            <div className="services__images-wrapper">
+                <img className="services__lines" src={Lines} alt="A woman falling through mess and cable on the floor" />
+                <img className="services__detail" src={Detail} alt="" />
             </div>
             <HouseCleaning />
             <CorporateCleaning />
