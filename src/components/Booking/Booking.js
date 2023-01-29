94b8e283d71675349abee62e6a98679123af8ccf
Dawid Kłos <klosdawid12@gmail.com>
2023-01-29 13:07:20 +0000

    Add basic HTML structure of Booking

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
+
 import StepOne from "./Steps/StepOne";
 import StepTwo from "./Steps/StepTwo";
+
+import UserIcon from "../../images/icons/icon-user.svg";
+import ChoiceIcon from "../../images/icons/icon-choice.svg";
+import DetailsIcon from "../../images/icons/icon-details.svg";
+import SuccessIcon from "../../images/icons/icon-success.svg";
+import Lines from "../../images/img-line-booking.svg";
+
+import "../../styles/booking.scss";
 
 const Booking = () => {
   const [currentStep, setCurrentStep] = useState(0);
@@ -15,18 +24,63 @@ const Booking = () => {
     },
   ];
 
+  const statusBarSteps = [
+    {
+      stepNumber: 0,
+      icon: UserIcon,
+      title: "Unternehmen",
+    },
+    {
+      stepNumber: 1,
+      icon: ChoiceIcon,
+      title: "Unsere Leistungen",
+    },
+    {
+      stepNumber: 2,
+      icon: DetailsIcon,
+      title: "Servicedetails",
+    },
+    {
+      stepNumber: 3,
+      icon: SuccessIcon,
+      title: "Senden",
+    },
+  ];
+
   return (
-    <section>
-      <form>
-        {formSteps.map((step, index) =>
-          index === currentStep ? (
-            <div key={step.stepNumber}>{step.component}</div>
+    <section className="booking">
+      <div className="booking__heading-wrapper">
+        <h2 className="booking__heading">Vereinbaren Sie einen Termin.</h2>
+        <p className="booking__desc">Wahlen Sie die Art Der Dienstleistungen und beantworten Sie dann einige Fragen.</p>
+      </div>
+
+      <div className="booking__steps-wrapper">
+        <img className="booking__lines" src={Lines} alt="" />
+        <div className="booking__steps">
+          <div className="booking__steps-bar">
+            {statusBarSteps.map((step) => (
+              <div className={`step ${step.stepNumber === currentStep ? "step--active" : ""}`} key={step.stepNumber}>
+                <div className="step__icon">
+                  <img src={step.icon} alt="" />
+                </div>
+                <span className="step__title">{step.title}</span>
+                <span className={`step__dot ${step.stepNumber === currentStep ? "step__dot--active" : ""}`}></span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <>
+        {formSteps.map((step) =>
+          step.stepNumber === currentStep ? (
+            <form className="booking__form" key={step.stepNumber}>
+              {step.component}
+            </form>
           ) : null
         )}
-      </form>
-      <button onClick={() => setCurrentStep((prevstate) => prevstate + 1)}>
-        Next
-      </button>
+      </>
+
+      <button onClick={() => setCurrentStep((prevstate) => prevstate + 1)}>Next</button>
     </section>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import * as React from "react";
 
-import AwesomeSlider from "react-awesome-slider";
-import "react-awesome-slider/dist/styles.css";
-
-import "../styles/index.scss";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Reviews from "../components/Reviews/Reviews";
 import Services from "../components/Services/Services";
 import Gallery from "../components/Gallery/Gallery";
 import Booking from "../components/Booking/Booking";
+
+import "../styles/index.scss";
 
 const IndexPage = () => {
   return (
@@ -23,6 +21,7 @@ const IndexPage = () => {
       <Services />
       <a id="gallery"></a>
       <Gallery />
+      <a id="form"></a>
       <Booking />
     </>
   );
