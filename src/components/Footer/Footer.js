0a6d16ab46fb9d1d6af5c559e980d82e04a16f99
Dawid Kłos <klosdawid12@gmail.com>
2023-02-02 15:15:07 +0000

    Add styles to Footer. Fix styles on Contact component.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Logo from "../../images/logo.svg";
+
+import "../../styles/footer.scss";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <img className="footer__img" src={Logo} alt="Rosa reinigungg company logo" />
+      <p className="footer__copyrights">Copyright © 2022 Rosa Reinigung. Alle Rechte vorbehalten.</p>
+    </div>
+  );
 };
 
 export default Footer;
