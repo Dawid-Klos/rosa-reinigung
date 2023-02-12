d1df6f91449b66545e76ec707a0b29f1067ee8ee
Dawid Kłos <klosdawid12@gmail.com>
2023-02-12 13:07:41 +0000

    fix: minor issues with footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,10 @@ import "../../styles/footer.scss";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer className="footer">
       <img className="footer__img" src={Logo} alt="Rosa reinigungg company logo" />
       <p className="footer__copyrights">Copyright © 2022 Rosa Reinigung. Alle Rechte vorbehalten.</p>
-    </div>
+    </footer>
   );
 };
 
