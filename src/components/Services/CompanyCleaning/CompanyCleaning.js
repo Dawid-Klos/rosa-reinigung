e36078a4a4dd438cb0e9f822b962915e62c357ce
Dawid Kłos <klosdawid12@gmail.com>
2023-02-13 13:14:48 +0000

    feat: add media queries to Services and each Service section

--- src/components/Services/CompanyCleaning/CompanyCleaning.js
+++ src/components/Services/CompanyCleaning/CompanyCleaning.js
@@ -4,11 +4,11 @@ import Service from "../Service";
 import ServiceImage from "../../../images/img-services-02.svg";
 import Lines from "../../../images/img-line-corporate.svg";
 
-const CorporateCleaning = () => {
+const CompanyCleaning = () => {
   const props = {
     title: "Reinigung von Büros, Arztpraxen, Theatern, Schulen usw.",
     description:
-      "Friseursalons,Schönheitssalons Bei Rosa Reinigung geht es nicht nur um die Reinigung und Organisation von Wohnungen. Mit uns können Sie jeden Raum Reinigen und Organisieren wir haben Erfahrung in der Reinigung von Büros, Hotels, Theatern,Schulen.. Wir sind offen für Ihren Vorschlag.",
+      "Friseursalons, Schönheitssalons Bei Rosa Reinigung geht es nicht nur um die Reinigung und Organisation von Wohnungen. Mit uns können Sie jeden Raum Reinigen und Organisieren wir haben Erfahrung in der Reinigung von Büros, Hotels, Theatern, Schulen. Wir sind offen für Ihren Vorschlag.",
     isReversed: true,
     image: ServiceImage,
     imageAlt: "A person vacuuming letters from the company floor",
@@ -20,4 +20,4 @@ const CorporateCleaning = () => {
   return <Service {...props} />;
 };
 
-export default CorporateCleaning;
+export default CompanyCleaning;
