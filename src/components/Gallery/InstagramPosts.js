bbc48978ebdec12f253dc1a74575e0547b865265
Dawid Kłos <klosdawid12@gmail.com>
2023-02-13 18:24:40 +0000

    feat: add media queries to Gallery section

--- src/components/Gallery/InstagramPosts.js
+++ src/components/Gallery/InstagramPosts.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import igPosts from "./posts.json";
+
 import { InstagramEmbed } from "react-social-media-embed";
 
 const InstagramPosts = igPosts.map((post) => {
   return (
-    <div className="gallery__post" key={post.id}>
-      <InstagramEmbed width={420} url={post.url} />
+    <div style={{ display: "flex", justifyContent: "center" }} className="gallery__post" key={post.id}>
+      <InstagramEmbed
+        style={{
+          maxWidth: "90vw",
+        }}
+        width="100%"
+        url={post.url}
+      />
     </div>
   );
 });
