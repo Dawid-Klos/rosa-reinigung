e889da21fce10dd47510e48ec67de340ae6866de
Dawid Klos <klosdawid12@gmail.com>
2023-03-14 19:48:49 +0000

    update: site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `Rosa Reinigung`,
     description: `Mit Hilfe von Rosa Reinigung können Sie Ihre Zeit für die wöchentliche Reinigung und die saisonale Fensterreinigung sparen und Ihren Umzug völlig stressfrei überstehen. Wir organisieren jeden Raum in Ihrem Haus, vom Dachboden bis zum Keller, damit Sauberkeit Ihr Dauergast ist.`,
     image: `/rosareinigung.png`,
-    siteUrl: `https://rosareinigungmain.gatsbyjs.io`,
+    siteUrl: `https://rosareinigung.de`,
     charset: `utf-8`,
   },
   plugins: [
@@ -31,13 +31,8 @@ module.exports = {
         start_url: "/",
         background_color: "#6b37bf",
         theme_color: "#6b37bf",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
+        icon: "src/images/icon.png",
         crossOrigin: `use-credentials`,
       },
     },
